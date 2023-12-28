7b489f9b88059aeb9a78227fa955c1776110e169
Namdevpatil <dev321.net@gmail.com>
2023-12-28 12:40:07 +0530

    DemoController added

---
+++ src/main/java/com/demo/DemoController.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+	
+	
+	@GetMapping("/")
+	public String hello()
+	{
+		return "Hello Buddy....!";
+	}
+
+}
